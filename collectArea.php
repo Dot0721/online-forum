b4890c04e284369bb833c5214fca95141f0bd34f
IanLin <dr.linian@gmail.com>
2024-05-27 19:07:18 +0800

    update style

--- collectArea.php
+++ collectArea.php
@@ -1,32 +1,35 @@
 <?php
-include "db.php";
-session_start();
+    include "db.php";
+
+    session_start();
     $userid = $_GET['userid'];
     $areaid = $_GET['areaid'];
     header("Location: viewAreaList.php?userid=$userid");
     $sql ="select * from collect_area where uid=$userid and aid=$areaid";
     $result = mysqli_query($db,$sql);
-    if($rows = mysqli_num_rows($result)==0){
+    if ($rows = mysqli_num_rows($result)==0){
         $sql = "INSERT INTO collect_area (uid, aid) VALUES ('$userid', '$areaid')";
-    if (!mysqli_query($db, $sql)) {
-        die('Error: ' . mysqli_error($db));
-    } else {
-        echo "
-         <script>
-         setTimeout(function(){window.location.href = 'viewAreaList.php?userid=$userid;},200);
-        </script>";
+        if (!mysqli_query($db, $sql)) {
+            die('Error: ' . mysqli_error($db));
+        }
+        else {
+            echo "
+            <script>
+                setTimeout(function(){window.location.href = 'viewAreaList.php?userid=$userid;},200);
+            </script>";
+        }
     }
-    }
-    else{
+    else {
         $sql = "DELETE FROM collect_area WHERE uid=$userid and aid=$areaid";
-    if (!mysqli_query($db, $sql)) {
-        die('Error: ' . mysqli_error($db));
-    } else {
-        echo "
-         <script>
-         setTimeout(function(){window.location.href = 'viewAreaList.php?userid=$userid;},200);
-        </script>";
-    }
+        if (!mysqli_query($db, $sql)) {
+            die('Error: ' . mysqli_error($db));
+        }
+        else {
+            echo "
+            <script>
+                setTimeout(function(){window.location.href = 'viewAreaList.php?userid=$userid;},200);
+            </script>";
+        }
     }
     // 假设 $db 是已连接的数据库连接对象
 ?>
