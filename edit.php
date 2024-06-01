f249e6e3fee35687e9db6657fed02414cf91072a
IanLin <dr.linian@gmail.com>
2024-06-01 14:36:50 +0800

    update style

--- edit.php
+++ edit.php
@@ -18,10 +18,36 @@
     }
 ?>
 
+<style>
+    .save {
+        width: 100px;
+        height: 50px;
+        color: white;
+        font-size: 16;
+        background: black;
+        border-radius: 5px;
+        position: relative;
+        top: 100px;
+        cursor: pointer;
+    }
+    .rewrite {
+        width: 100px;
+        height: 50px;
+        color: white;
+        font-size: 16;
+        background: black;
+        border-radius: 5px;
+        position: relative;
+        top: 100px;
+        cursor: pointer;
+    }
+</style>
+
 <body>
-    <a href='viewPostDetail.php?postid=<?=$postid?>&userid=<?=$userid?>&areaid=<?=$areaid?>'> <button class='last-page'> <b> Last Page </b> </button> </a>
     <a href='viewAreaList.php?userid=". $userid . "'> <button class='bubbles'> <b> Bubbles </b> </button> </a>
 	<a href="index.php"> <button class="upper-right-button"> <b> Log Out </b> </button> </a>
+    <a href='userinfo.php?userid=" . $userid . "&areaid=0&postid=0'> <button class='account'> <b> Account </b> </button> </a>
+    <a href='viewPostDetail.php?postid=<?=$postid?>&userid=<?=$userid?>&areaid=<?=$areaid?>'> <button class='last-page'> <b> Last Page </b> </button> </a>
     <?php
         include 'db.php';
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
@@ -33,39 +59,22 @@
         $result = mysqli_query($db, $query);
         while ($rs = mysqli_fetch_array($result)) {
     ?>
-      <div class="content">
-                <div class="m-b-md">
-                    <form name="form1" action="edit.php" method="post">
-                        <input type="hidden" name="postid" value="<?=$rs['postid']?>">
-                        <input type="hidden" name="userid" value="<?=$userid?>">
-                        <input type="hidden" name="areaid" value="<?=$areaid?>">
-                        <p>TITLE</p>
-                        <input type="text" name="postname" value="<?=$rs['postname']?>">
-                        <p>ARTICLE</p>
-                        <p><textarea style="font-family: 'Nunito', sans-serif; font-size:20px; width:550px; height:100px;" name="article"></textarea></p>
-                        <p><input type="submit" name="submit" value="SAVE">
-                    <style>
-                        input {padding:5px 15px; background:#ccc; border:0 none;
-                        cursor:pointer;
-                        -webkit-border-radius: 5px;
-                        border-radius: 5px; }
-                    </style>
-                        <input type="reset" name="Reset" value="REWRITE">
-                    <style>
-                        input {
-                            padding:5px 15px;
-                            background:#FFCCCC;
-                            border:0 none;
-                            cursor:pointer;
-                            -webkit-border-radius: 5px;
-                            border-radius: 5px;
-                            font-family: 'Nunito', sans-serif;
-                            font-size: 19px;
-                        }
-                    </style>
-                    </form>
-                </div>
-
+    <form name="form1" action="edit.php" method="post">
+        <div style="text-align: center;">
+            <input type="hidden" name="postid" value="<?=$rs['postid']?>">
+            <input type="hidden" name="userid" value="<?=$userid?>">
+            <input type="hidden" name="areaid" value="<?=$areaid?>">
+            <h1> Edit Post </h1>
+            <p class="dir"> What content do you want to modified? </p>
+            <h2> Postname </h2>
+            <input type="text" name="postname" value="<?=$rs['postname']?>" class="input-field">
+            <h2> Article </h2>
+            <textarea name="article"><?= htmlspecialchars($rs['article']) ?></textarea>
+            <br>
+            <button type="submit" name="submit" value="SAVE" class="save"> <b> Save </b> </button>
+            <button type="reset" name="Reset" value="REWRITE" class="rewrite"> <b> Rewrite </b> </button>
+        </div>
+    </form>
 </body>
 
 </html>
@@ -88,7 +97,9 @@
 
         }
     }
+    /*
     else {
         echo '<div class="success">Click <strong>Send</strong> when you\'re done.</div>';
     }
+    */
 ?>
