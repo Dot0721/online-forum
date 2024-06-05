7444c9afe75991511985d09095ba2866a0847934
Dot0721 <dot.point.spot@gmail.com>
2024-06-05 14:49:25 +0800

    searching area function

--- findarea.php
+++ findarea.php
@@ -1,17 +1,31 @@
 <html>
-	
-<title> All Areas </title>
-	
+
+<title>Areas </title>
+
 <?php
-	include "style.html";
+    include "style.html";
 	include "db.php";
-	$userid = $_GET['userid'];
 ?>
 
 <style>
-	h3 {
-		position: relative;
-		left: 0;
+	.main {
+		width: 500px;
+        font-size: 20px;
+		font-family: 'Nunito', sans-serif;
+        letter-spacing: .125rem;
+        position: relative;
+        left: 400px;
+        top: 125px;
+	}
+	.enter {
+		width: 90px;
+        height: 35px;
+		color: white;
+        background: black;
+        border-radius: 5px;
+		position: absolute;
+		right: 0px;
+		cursor: pointer;
 	}
 	a {
 		text-decoration: none;
@@ -48,68 +62,63 @@
 		position: relative;
 		bottom: -3em;
 	}
+	.fit {
+		width: 100%;
+		height: 100%;
+	}
 	.centerbox {
 		display: flex;
 		justify-content: center;
 	}
 </style>
-
 <body>
 	<?php
+    $userid = $_GET['userid'];
+    $search = $_GET['search'];
 		// Toolbar for non-member
 		if (!$userid) {
-			echo "<a href='viewAreaList.php?userid=0'> <button class='bubbles'> <b> Bubbles </b> </button> </a>";
-			echo '<a href="index.php"> <button class="upper-right-button"> <b> Login </b> </button> </a>';
+			echo '<a href="index.php"> Login </a>';
 		}
 		// Toolbar for member
 		else {
-			// Get user info from db
 			$sql ="select * from register_user where userid=$userid";
 			$result = mysqli_query($db,$sql);
 			$row = mysqli_fetch_assoc($result);
-			echo "<a href='viewAreaList.php?userid=".$userid."'> <button class='bubbles'> <b> Bubbles </b> </button> </a>";
-			echo '<form action="findarea.php" method="get">
+			echo "<a href='viewAreaList.php?userid=". $userid . "'> <button class='bubbles'> <b> Bubbles </b> </button> </a>";
+            echo '<form action="findarea.php" method="get">
 					<input type="hidden" name="userid" value="'.$userid.'">
 					<p><input type="text" name="search"></p>
 					<button type="submit">Search</button></form>';
 			echo '<a href="index.php"> <button class="upper-right-button"> <b> Log Out </b> </button> </a>';
 			echo "<a href='userinfo.php?userid=" . $userid . "&areaid=0&postid=0'> <button class='account'> <b> Account </b> </button> </a>";
-			echo "<a href='collectAreaList.php?userid=" . $userid . "'> <button class='fav'> <b> Favorite </b> </button> </a>";
-			// Give access to create area if admin
-			if($row['permission_level']==3){
+			/*if($row['permission_level']==3){
 				echo "<a href='createArea.php?userid=" . $userid . "'> <button class='create-area'> <b> Create Area </b> </button> </a>";
-			}
+			}*/
 		}
 	?>
-	<!-- Heading -->
-	<h1> All Areas </h1>
+	<a href='viewAreaList.php?userid=<?=$userid?>'> <button class='last-page'> <b> All Areas </b> </button> </a>
+	<h1> Favorite Areas </h1>
 	<p class="dir"> Choose an area to start chatting! </p>
 	<div class="centerbox">
 		<div class="cards">
 			<?php
-				// Find area info from db
-				$sql = "select * from post_area";
+				$sql = "SELECT * 
+				FROM post_area pa
+				WHERE pa.areaname LIKE '%$search%'";
 				$result = mysqli_query($db, $sql);
-				// Show every area
+				//從資料庫中撈留言紀錄並顯示出來
 				while ($row = mysqli_fetch_assoc($result)) {
 					$areaname=$row['areaname'];
 					$areaid=$row['areaid'];
 					// Container for each area
 					echo "<div class='box'>";
-					echo 	"<h3 style='height:13%; width:85%'> $areaname </h3>";
-					// Check if area is favorated by user
-					if ($userid) {
-						$sql = "select COUNT(*) as fav from collect_area where uid=$userid AND aid=$areaid";
-						$is_favCount = mysqli_query($db, $sql);
-						$is_fav = mysqli_fetch_assoc($is_favCount)['fav'] != 0;
-						// Choose image(star) to display
-						$star_style = "icon/star-" . ($is_fav ? "black" : "hollow") . ".svg" ;
-						echo 	"<a href='collectArea.php?areaid=$areaid&userid=$userid' class='star icon-btn'> <img src=$star_style alt='Favorite' class='fit'	> </a>";
-					}
+					echo 	"<h3 style='height:13%; width:85%; text-align:center'> $areaname </h3>";
+					$star_style = "icon/star-black.svg" ;
+					echo 	"<a href='collectArea.php?areaid=$areaid&userid=$userid' class='star icon-btn'> <img src=$star_style alt='Favorite' class='fit'	> </a>";
 					// Button to enter area
 					echo 	"<div class='centerbox'>
 								<a href='viewPostList.php?areaid=$areaid&userid=$userid' class='enter'> enter </a>
-						  	</div>";
+							</div>";
 					echo "</div>";
 				}
 			?>
